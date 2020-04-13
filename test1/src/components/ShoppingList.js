48045c1df30fa251a401b3e49974148078f25ea0
tani <tani@1-10.com>
2020-04-13 17:24:07 +0900

    [update]不要ファイル削除、prettierrc追加

---
+++ test1/src/components/ShoppingList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class ShoppingList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return <ShoppingList></ShoppingList>
+  }
+}
+
+export default ShoppingList
