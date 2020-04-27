b7c40486f95bf46aa365685bb4d3013808997ccc
tani <tani@1-10.com>
2020-04-27 11:43:42 +0900

    [update]

--- test1/src/components/Board.js
+++ test1/src/components/Board.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
-import Square from './Square'
+import React, { Component } from "react"
+import Square from "./Square"
 
 class Board extends Component {
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    if(calculateWinner(squares) || squares[i]) {
+      return
+    }
+    const xIsNext = this.state.xIsNext
+    squares[i] = xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !xIsNext
+    })
+  }
+
   renderSquare(i) {
-    return <Square value={i} />
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    )
   }
 
   render() {
-    const status = 'Next player: X'
+    const winner = calculateWinner(this.state.squares)
+    
+    let status
+    if(winner) {
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    }
 
     return (
       <div>
@@ -31,5 +56,25 @@ class Board extends Component {
     )
   }
 }
- 
-export default Board;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Board
